633bf10210d07ebdf4f751a72e61f18ce3a0c240
root <root@debian>
2013-12-04 17:50:12 +0100

    update

---
+++ PJ/uboot_kernel/u-boot-sunxi/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Oct 18 2013"
+#define U_BOOT_TIME "11:34:52"
---
+++ PJ/uboot_kernel/u-boot-sunxi/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2013.10-rc2-08400-g8a4621c"
+#define U_BOOT_VERSION "U-Boot 2013.10-rc2-08400-g8a4621c"
+#define CC_VERSION_STRING "arm-linux-gnueabihf-gcc (crosstool-NG linaro-1.13.1-4.7-2013.01-20130125 - Linaro GCC 2013.01) 4.7.3 20130102 (prerelease)"
+#define LD_VERSION_STRING "GNU ld (crosstool-NG linaro-1.13.1-4.7-2013.01-20130125 - Linaro GCC 2013.01) 2.22"

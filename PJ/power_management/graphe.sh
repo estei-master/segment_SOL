9aacbf94c029582d8b202ee7bb4ff72963f9fd02
root <root@debian>
2013-12-10 23:51:47 +0100

    update

--- PJ/power_management/graphe.sh
+++ PJ/power_management/graphe.sh
@@ -2,7 +2,8 @@
 
 # Author : Pierre-jean TEXIER
 # Date   : December 2013
-
+# Goal	 : Generate .png with gnuplot
+ 
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
@@ -21,13 +22,24 @@
 # Variables #
 NAME='power_management.png'
 
-echo "set terminal png size 1024,600 ; set xrange [0:40] ; set xlabel 'Temps' ; set ylabel '% Batterie' ; set title 'Consommation Batterie sur 34 min' ; set output './$NAME'; set multiplot layout 2,1 ;
+function usage() {
+cat << EOF
+Usage:
+
+$0 [file_1 --> ex : LOG] [file_2 --> ex : LOG2]
+	
+EOF
+}
+
+
+echo "set terminal png size 1024,600 ; set xrange [0:40] ; set xlabel 'Temps' ; set ylabel '% Batterie' ; set title 'Consommation Batterie' ; set output './$NAME'; set multiplot layout 2,1 ;
 	plot '$1' using 1:2 with linespoints title 'Sans Appli Qt'  ; 
 	plot '$2' using 1:2 with linespoints lc rgb '#000000FF' title 'Avec Appli Qt'" | gnuplot
 
 if [ $? != 0 ]  
 then
 	echo "Echec de la création de $NAME"
+	usage
 else
 	echo "Création de $NAME réussie"
 fi
